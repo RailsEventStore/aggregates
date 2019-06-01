af64b361d2c1624e6e9bff86fb8a470d9d0d5c82
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2019-06-01 09:33:22 +0300

    ActiveRecord-based implementation

---
+++ rails_way/project_management/command_handler.rb
@@ -0,0 +1,85 @@
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:'
+)
+
+ActiveRecord::Schema.define do
+  create_table :issues, force: true do |t|
+    t.string :uuid
+    t.string :state
+  end
+end
+
+module ProjectManagement
+
+  class CommandHandler
+    def initialize(event_store)
+      @event_store = event_store
+    end
+
+    def create(cmd)
+      load_issue(cmd.id) do |issue|
+        issue.open
+        IssueOpened.new(data: {issue_id: cmd.id})
+      end
+    end
+
+    def close(cmd)
+      load_issue(cmd.id) do |issue|
+        issue.close
+        IssueClosed.new(data: {issue_id: cmd.id})
+      end
+    end
+
+    def start(cmd)
+      load_issue(cmd.id) do |issue|
+        issue.start
+        IssueProgressStarted.new(data: {issue_id: cmd.id})
+      end
+    end
+
+    def stop(cmd)
+      load_issue(cmd.id) do |issue|
+        issue.stop
+        IssueProgressStopped.new(data: {issue_id: cmd.id})
+      end
+    end
+
+    def reopen(cmd)
+      load_issue(cmd.id) do |issue|
+        issue.reopen
+        IssueReopened.new(data: {issue_id: cmd.id})
+      end
+    end
+
+    def resolve(cmd)
+      load_issue(cmd.id) do |issue|
+        issue.resolve
+        IssueResolved.new(data: {issue_id: cmd.id})
+      end
+    end
+
+    private
+
+    def stream_name(id)
+      "Issue$#{id}"
+    end
+
+    def load_issue(id)
+      issue = Issue.find_or_create_by(uuid: id)
+      events = yield issue
+      issue.save!
+      publish(events, id)
+    end
+
+    def publish(events, id)
+      @event_store.publish(
+        events,
+        stream_name: stream_name(id),
+      )
+    end
+  end
+end
---
+++ rails_way/project_management/issue.rb
@@ -0,0 +1,86 @@
+module ProjectManagement
+
+  class Issue < ActiveRecord::Base
+    class InvalidTransition < StandardError; end
+
+    def open
+      invalid_transition unless can_create?
+      self.state = "open"
+    end
+
+    def resolve
+      invalid_transition unless can_resolve?
+      self.state = "resolved"
+    end
+
+    def close
+      invalid_transition unless can_close?
+      self.state = "closed"
+    end
+
+    def reopen
+      invalid_transition unless can_reopen?
+      self.state = "reopened"
+    end
+
+    def start
+      invalid_transition unless can_start?
+      self.state = "in_progress"
+    end
+
+    def stop
+      invalid_transition unless can_stop?
+      self.state = "open"
+    end
+
+    private
+
+    def invalid_transition
+      raise InvalidTransition
+    end
+
+    def open?
+      state == "open"
+    end
+
+    def closed?
+      state == "closed"
+    end
+
+    def in_progress?
+      state == "in_progress"
+    end
+
+    def reopened?
+      state == "reopened"
+    end
+
+    def resolved?
+      state == "resolved"
+    end
+
+    def can_reopen?
+      closed? || resolved?
+    end
+
+    def can_start?
+      open? || reopened?
+    end
+
+    def can_stop?
+      in_progress?
+    end
+
+    def can_close?
+      open? || in_progress? || reopened? || resolved?
+    end
+
+    def can_resolve?
+      open? || reopened? || in_progress?
+    end
+
+    def can_create?
+      !open?
+    end
+  end
+end
