05bdc86414ed57f5330a217440e87c021d5198c4
Paweł Pacana <pawel.pacana@gmail.com>
2023-10-20 22:04:10 +0200

    Endless methods here as well

--- aggregate_root/project_management/command_handler.rb
+++ aggregate_root/project_management/command_handler.rb
@@ -4,40 +4,17 @@ module ProjectManagement
       @repository = AggregateRoot::Repository.new(event_store)
     end
 
-    def create(cmd)
-      with_aggregate(cmd.id) { |issue| issue.open}
-    end
-
-    def resolve(cmd)
-      with_aggregate(cmd.id) { |issue| issue.resolve }
-    end
-
-    def close(cmd)
-      with_aggregate(cmd.id) { |issue| issue.close }
-    end
-
-    def reopen(cmd)
-      with_aggregate(cmd.id) { |issue| issue.reopen }
-    end
-
-    def start(cmd)
-      with_aggregate(cmd.id) { |issue| issue.start }
-    end
-
-    def stop(cmd)
-      with_aggregate(cmd.id) { |issue| issue.stop }
-    end
+    def create(cmd) = with_aggregate(cmd.id) { |issue| issue.open }
+    def resolve(cmd) = with_aggregate(cmd.id) { |issue| issue.resolve }
+    def close(cmd) = with_aggregate(cmd.id) { |issue| issue.close }
+    def reopen(cmd) = with_aggregate(cmd.id) { |issue| issue.reopen }
+    def start(cmd) = with_aggregate(cmd.id) { |issue| issue.start }
+    def stop(cmd) = with_aggregate(cmd.id) { |issue| issue.stop }
 
     private
 
-    attr_reader :repository
-
-    def stream_name(id)
-      "Issue$#{id}"
-    end
-
     def with_aggregate(id, &block)
-      repository.with_aggregate(Issue.new(id), stream_name(id), &block)
+      @repository.with_aggregate(Issue.new(id), "Issue$#{id}", &block)
     end
   end
 end
