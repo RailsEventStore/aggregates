2d708d6ff3a629926bcf1d4c8ba39a6c5416348e
Paweł Pacana <pawel.pacana@gmail.com>
2023-10-23 22:45:15 +0200

    Inline

--- shared/lib/project_management.rb
+++ shared/lib/project_management.rb
@@ -1,3 +1,29 @@
-require_relative "project_management/commands"
-require_relative "project_management/events"
-require_relative "project_management/configuration"
+require "ruby_event_store"
+
+module ProjectManagement
+  CreateIssue = Data.define(:id)
+  ResolveIssue = Data.define(:id)
+  CloseIssue = Data.define(:id)
+  ReopenIssue = Data.define(:id)
+  StartIssueProgress = Data.define(:id)
+  StopIssueProgress = Data.define(:id)
+
+  IssueOpened = Class.new(RubyEventStore::Event)
+  IssueResolved = Class.new(RubyEventStore::Event)
+  IssueClosed = Class.new(RubyEventStore::Event)
+  IssueReopened = Class.new(RubyEventStore::Event)
+  IssueProgressStarted = Class.new(RubyEventStore::Event)
+  IssueProgressStopped = Class.new(RubyEventStore::Event)
+
+  class Configuration
+    def call(event_store, command_bus)
+      handler = CommandHandler.new(event_store)
+      command_bus.register(CreateIssue, handler.public_method(:create))
+      command_bus.register(ReopenIssue, handler.public_method(:reopen))
+      command_bus.register(ResolveIssue, handler.public_method(:resolve))
+      command_bus.register(CloseIssue, handler.public_method(:close))
+      command_bus.register(StartIssueProgress, handler.public_method(:start))
+      command_bus.register(StopIssueProgress, handler.public_method(:stop))
+    end
+  end
+end
--- shared/lib/project_management/commands.rb
+++
@@ -1,8 +0,0 @@
-module ProjectManagement
-  CreateIssue = Data.define(:id)
-  ResolveIssue = Data.define(:id)
-  CloseIssue = Data.define(:id)
-  ReopenIssue = Data.define(:id)
-  StartIssueProgress = Data.define(:id)
-  StopIssueProgress = Data.define(:id)
-end
