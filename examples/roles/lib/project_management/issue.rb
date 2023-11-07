4fb9a3088d8f60cf1e243c402e99e62692d8a450
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-07 12:23:26 +0100

    Refactor

--- examples/roles/lib/project_management/issue.rb
+++ examples/roles/lib/project_management/issue.rb
@@ -1,46 +1,49 @@
 module ProjectManagement
   class Issue
     def open = extend(Open.clone)
-    def start = raise InvalidTransition
-    def resolve = raise InvalidTransition
-    def stop = raise InvalidTransition
-    def reopen = raise InvalidTransition
-    def close = raise InvalidTransition
+    def start = fail
+    def resolve = fail
+    def stop = fail
+    def reopen = fail
+    def close = fail
+
+    private
+    def fail = raise InvalidTransition
   end
 
   module Open
-    def open = raise Issue::InvalidTransition
+    def open = fail
     def start = extend(InProgress.clone)
     def resolve = extend(Resolved.clone)
-    def stop = raise Issue::InvalidTransition
-    def reopen = raise Issue::InvalidTransition
+    def stop = fail
+    def reopen = fail
     def close = extend(Closed.clone)
   end
 
   module InProgress
-    def open = raise Issue::InvalidTransition
-    def start = raise Issue::InvalidTransition
+    def open = fail
+    def start = fail
     def resolve = extend(Resolved.clone)
     def stop = extend(Open.clone)
-    def reopen = raise Issue::InvalidTransition
+    def reopen = fail
     def close = extend(Closed.clone)
   end
 
   module Resolved
-    def open = raise Issue::InvalidTransition
-    def start = raise Issue::InvalidTransition
-    def resolve = raise Issue::InvalidTransition
-    def stop = raise Issue::InvalidTransition
+    def open = fail
+    def start = fail
+    def resolve = fail
+    def stop = fail
     def reopen = extend(Open.clone)
     def close = extend(Closed.clone)
   end
 
   module Closed
-    def open = raise Issue::InvalidTransition
-    def start = raise Issue::InvalidTransition
-    def resolve = raise Issue::InvalidTransition
-    def stop = raise Issue::InvalidTransition
+    def open = fail
+    def start = fail
+    def resolve = fail
+    def stop = fail
     def reopen = extend(Open.clone)
-    def close = raise Issue::InvalidTransition
+    def close = fail
   end
 end
