e201f3e464d2335aa97d570973317397bc5cce51
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-22 12:22:44 +0100

    Initial decider implementation
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>
    Co-authored-by: Piotr Jurewicz <pjurewicz93@gmail.com>

--- examples/decider/lib/project_management/command_handler.rb
+++ examples/decider/lib/project_management/command_handler.rb
@@ -2,36 +2,36 @@ module ProjectManagement
   class CommandHandler
     def initialize(event_store) = @event_store = event_store
 
-    def create(cmd) = with_state(cmd.id) { |state| Issue.open(state) }
-    def resolve(cmd) = with_state(cmd.id) { |state| Issue.resolve(state) }
-    def close(cmd) = with_state(cmd.id) { |state| Issue.close(state) }
-    def reopen(cmd) = with_state(cmd.id) { |state| Issue.reopen(state) }
-    def start(cmd) = with_state(cmd.id) { |state| Issue.start(state) }
-    def stop(cmd) = with_state(cmd.id) { |state| Issue.stop(state) }
+    def create(cmd) = with_event_store(cmd)
+    def resolve(cmd) = with_event_store(cmd)
+    def close(cmd) = with_event_store(cmd)
+    def reopen(cmd) = with_event_store(cmd)
+    def start(cmd) = with_event_store(cmd)
+    def stop(cmd) = with_event_store(cmd)
 
     private
 
     def stream_name(id) = "Issue$#{id}"
 
-    def with_state(id)
+    def with_event_store(cmd)
       state, version =
         @event_store
           .read
-          .stream(stream_name(id))
-          .reduce([IssueState.initial(id), -1]) do |(state, version), event|
-            [state.apply(event), version + 1]
+          .stream(stream_name(cmd.id))
+          .reduce(
+            [Issue.initial_state(cmd.id), -1]
+          ) do |(state, version), event|
+            [Issue.evolve(state, event), version + 1]
           end
 
-      case result = yield(state)
-      when Issue::InvalidTransition
-        raise Error
-      else
-        @event_store.publish(
-          result,
-          stream_name: stream_name(id),
-          expected_version: version
-        )
-      end
+      events = Issue.decide(cmd, state)
+      raise Error if events.empty?
+
+      @event_store.publish(
+        events,
+        stream_name: stream_name(cmd.id),
+        expected_version: version
+      )
     end
   end
 end
--- examples/decider/lib/project_management/issue.rb
+++ examples/decider/lib/project_management/issue.rb
@@ -1,53 +1,55 @@
 module ProjectManagement
   module Issue
-    InvalidTransition = Class.new(StandardError)
+    State = Data.define(:id, :status)
 
-    def self.open(state)
-      if state.status
-        InvalidTransition.new
-      else
-        IssueOpened.new(data: { issue_id: state.id })
+    def self.decide(command, state)
+      events =
+        case command
+        when CreateIssue
+          [IssueOpened.new(data: { issue_id: state.id })] unless state.status
+        when ResolveIssue
+          if %i[open in_progress reopened].include? state.status
+            [IssueResolved.new(data: { issue_id: state.id })]
+          end
+        when CloseIssue
+          if %i[open in_progress resolved reopened].include? state.status
+            [IssueClosed.new(data: { issue_id: state.id })]
+          end
+        when ReopenIssue
+          if %i[resolved closed].include? state.status
+            [IssueReopened.new(data: { issue_id: state.id })]
+          end
+        when StartIssueProgress
+          if %i[open reopened].include? state.status
+            [IssueProgressStarted.new(data: { issue_id: state.id })]
+          end
+        when StopIssueProgress
+          if %i[in_progress].include? state.status
+            [IssueProgressStopped.new(data: { issue_id: state.id })]
+          end
+        end
+      Array(events)
+    end
+
+    def self.evolve(state, event)
+      case event
+      when IssueOpened
+        state.with(status: :open)
+      when IssueResolved
+        state.with(status: :resolved)
+      when IssueClosed
+        state.with(status: :closed)
+      when IssueReopened
+        state.with(status: :reopened)
+      when IssueProgressStarted
+        state.with(status: :in_progress)
+      when IssueProgressStopped
+        state.with(status: :open)
       end
     end
 
-    def self.resolve(state)
-      if %i[open in_progress reopened].include? state.status
-        IssueResolved.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
-      end
-    end
-
-    def self.close(state)
-      if %i[open in_progress resolved reopened].include? state.status
-        IssueClosed.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
-      end
-    end
-
-    def self.reopen(state)
-      if %i[resolved closed].include? state.status
-        IssueReopened.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
-      end
-    end
-
-    def self.stop(state)
-      if %i[in_progress].include? state.status
-        IssueProgressStopped.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
-      end
-    end
-
-    def self.start(state)
-      if %i[open reopened].include? state.status
-        IssueProgressStarted.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
-      end
+    def self.initial_state(id)
+      State.new(id: id, status: nil)
     end
   end
 end
