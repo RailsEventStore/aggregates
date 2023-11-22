700bbac6c7ef8f7215ab3b54570c81a9ea9b1496
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-22 15:08:00 +0100

    Revert "Extract state transitions"
    This reverts commit 1d0945681300a2faf406ac91258c9415195192e8.

--- examples/decider/lib/project_management/issue.rb
+++ examples/decider/lib/project_management/issue.rb
@@ -11,17 +11,41 @@ module ProjectManagement
     def decide(command)
       case command
       when CreateIssue
-        open
+        if @state.status
+          InvalidTransition.new
+        else
+          IssueOpened.new(data: { issue_id: @state.id })
+        end
       when ResolveIssue
-        resolve
+        if %i[open in_progress reopened].include? @state.status
+          IssueResolved.new(data: { issue_id: @state.id })
+        else
+          InvalidTransition.new
+        end
       when CloseIssue
-        close
+        if %i[open in_progress resolved reopened].include? @state.status
+          IssueClosed.new(data: { issue_id: @state.id })
+        else
+          InvalidTransition.new
+        end
       when ReopenIssue
-        reopen
+        if %i[resolved closed].include? @state.status
+          IssueReopened.new(data: { issue_id: @state.id })
+        else
+          InvalidTransition.new
+        end
       when StartIssueProgress
-        start
+        if %i[open reopened].include? @state.status
+          IssueProgressStarted.new(data: { issue_id: @state.id })
+        else
+          InvalidTransition.new
+        end
       when StopIssueProgress
-        stop
+        if %i[in_progress].include? @state.status
+          IssueProgressStopped.new(data: { issue_id: @state.id })
+        else
+          InvalidTransition.new
+        end
       end
     end
 
@@ -44,54 +68,6 @@ module ProjectManagement
     end
 
     private
-
-    def open
-      if @state.status
-        InvalidTransition.new
-      else
-        IssueOpened.new(data: { issue_id: @state.id })
-      end
-    end
-
-    def resolve
-      if %i[open in_progress reopened].include? @state.status
-        IssueResolved.new(data: { issue_id: @state.id })
-      else
-        InvalidTransition.new
-      end
-    end
-
-    def close
-      if %i[open in_progress resolved reopened].include? @state.status
-        IssueClosed.new(data: { issue_id: @state.id })
-      else
-        InvalidTransition.new
-      end
-    end
-
-    def reopen
-      if %i[resolved closed].include? @state.status
-        IssueReopened.new(data: { issue_id: @state.id })
-      else
-        InvalidTransition.new
-      end
-    end
-
-    def stop
-      if %i[in_progress].include? @state.status
-        IssueProgressStopped.new(data: { issue_id: @state.id })
-      else
-        InvalidTransition.new
-      end
-    end
-
-    def start
-      if %i[open reopened].include? @state.status
-        IssueProgressStarted.new(data: { issue_id: @state.id })
-      else
-        InvalidTransition.new
-      end
-    end
 
     def initial_state(id) = State.new(id: id, status: nil)
   end
