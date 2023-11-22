8f9f6aad6701ff36af56dcec655cb2a1b9349f2f
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-22 11:45:22 +0100

    There there
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>
    Co-authored-by: Piotr Jurewicz <pjurewicz93@gmail.com>

--- examples/extracted_state/lib/project_management/issue.rb
+++ examples/extracted_state/lib/project_management/issue.rb
@@ -11,32 +11,32 @@ module ProjectManagement
 
     def open
       fail if @state.status
-      @changes << IssueOpened.new(data: { issue_id: @state.id })
+      changes << IssueOpened.new(data: { issue_id: @state.id })
     end
 
     def resolve
       fail unless %i[open in_progress reopened].include? @state.status
-      @changes << IssueResolved.new(data: { issue_id: @state.id })
+      changes << IssueResolved.new(data: { issue_id: @state.id })
     end
 
     def close
       fail unless %i[open in_progress resolved reopened].include? @state.status
-      @changes << IssueClosed.new(data: { issue_id: @state.id })
+      changes << IssueClosed.new(data: { issue_id: @state.id })
     end
 
     def reopen
       fail unless %i[resolved closed].include? @state.status
-      @changes << IssueReopened.new(data: { issue_id: @state.id })
+      changes << IssueReopened.new(data: { issue_id: @state.id })
     end
 
     def start
       fail unless %i[open reopened].include? @state.status
-      @changes << IssueProgressStarted.new(data: { issue_id: @state.id })
+      changes << IssueProgressStarted.new(data: { issue_id: @state.id })
     end
 
     def stop
       fail unless %i[in_progress].include? @state.status
-      @changes << IssueProgressStopped.new(data: { issue_id: @state.id })
+      changes << IssueProgressStopped.new(data: { issue_id: @state.id })
     end
 
     def fail = raise InvalidTransition
