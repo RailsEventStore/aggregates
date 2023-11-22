1d0945681300a2faf406ac91258c9415195192e8
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-22 14:07:13 +0100

    Extract state transitions
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>
    Co-authored-by: Piotr Jurewicz <pjurewicz93@gmail.com>

--- examples/decider/lib/project_management/issue.rb
+++ examples/decider/lib/project_management/issue.rb
@@ -11,41 +11,17 @@ module ProjectManagement
     def decide(command)
       case command
       when CreateIssue
-        if @state.status
-          InvalidTransition.new
-        else
-          IssueOpened.new(data: { issue_id: @state.id })
-        end
+        open
       when ResolveIssue
-        if %i[open in_progress reopened].include? @state.status
-          IssueResolved.new(data: { issue_id: @state.id })
-        else
-          InvalidTransition.new
-        end
+        resolve
       when CloseIssue
-        if %i[open in_progress resolved reopened].include? @state.status
-          IssueClosed.new(data: { issue_id: @state.id })
-        else
-          InvalidTransition.new
-        end
+        close
       when ReopenIssue
-        if %i[resolved closed].include? @state.status
-          IssueReopened.new(data: { issue_id: @state.id })
-        else
-          InvalidTransition.new
-        end
+        reopen
       when StartIssueProgress
-        if %i[open reopened].include? @state.status
-          IssueProgressStarted.new(data: { issue_id: @state.id })
-        else
-          InvalidTransition.new
-        end
+        start
       when StopIssueProgress
-        if %i[in_progress].include? @state.status
-          IssueProgressStopped.new(data: { issue_id: @state.id })
-        else
-          InvalidTransition.new
-        end
+        stop
       end
     end
 
@@ -68,6 +44,54 @@ module ProjectManagement
     end
 
     private
+
+    def open
+      if @state.status
+        InvalidTransition.new
+      else
+        IssueOpened.new(data: { issue_id: @state.id })
+      end
+    end
+
+    def resolve
+      if %i[open in_progress reopened].include? @state.status
+        IssueResolved.new(data: { issue_id: @state.id })
+      else
+        InvalidTransition.new
+      end
+    end
+
+    def close
+      if %i[open in_progress resolved reopened].include? @state.status
+        IssueClosed.new(data: { issue_id: @state.id })
+      else
+        InvalidTransition.new
+      end
+    end
+
+    def reopen
+      if %i[resolved closed].include? @state.status
+        IssueReopened.new(data: { issue_id: @state.id })
+      else
+        InvalidTransition.new
+      end
+    end
+
+    def stop
+      if %i[in_progress].include? @state.status
+        IssueProgressStopped.new(data: { issue_id: @state.id })
+      else
+        InvalidTransition.new
+      end
+    end
+
+    def start
+      if %i[open reopened].include? @state.status
+        IssueProgressStarted.new(data: { issue_id: @state.id })
+      else
+        InvalidTransition.new
+      end
+    end
 
     def initial_state(id) = State.new(id: id, status: nil)
   end
