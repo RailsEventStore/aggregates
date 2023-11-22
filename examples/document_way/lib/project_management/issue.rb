9edfa9324ad66e55eac2eb89eb8332aa29661efe
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-22 11:31:17 +0100

    There, fixed it
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>
    Co-authored-by: Piotr Jurewicz <pjurewicz93@gmail.com>

--- examples/document_way/lib/project_management/issue.rb
+++ examples/document_way/lib/project_management/issue.rb
@@ -28,37 +28,37 @@ module ProjectManagement
 
     def open
       fail if @state.status
-      @state = "open"
+      @state.status = "open"
       IssueOpened.new(data: { issue_id: @state.id })
     end
 
     def resolve
       fail unless %w[open in_progress reopened].include? @state.status
-      @state = "resolved"
+     @state.status = "resolved"
       IssueResolved.new(data: { issue_id: @state.id })
     end
 
     def close
       fail unless %w[open in_progress resolved reopened].include? @state.status
-      @state = "closed"
+     @state.status = "closed"
       IssueClosed.new(data: { issue_id: @state.id })
     end
 
     def reopen
       fail unless %w[resolved closed].include? @state.status
-      @state = "reopened"
+     @state.status = "reopened"
       IssueReopened.new(data: { issue_id: @state.id })
     end
 
     def start
       fail unless %w[open reopened].include? @state.status
-      @state = "in_progress"
+     @state.status = "in_progress"
       IssueProgressStarted.new(data: { issue_id: @state.id })
     end
 
     def stop
       fail unless %w[in_progress].include? @state.status
-      @state = "open"
+     @state.status = "open"
       IssueProgressStopped.new(data: { issue_id: @state.id })
     end
 
