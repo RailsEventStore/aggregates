28c9a503bcbe99b94727c9a6dc9f011684f715d2
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-22 15:14:48 +0100

    Take advantage of eigenclass to match decider example
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>
    Co-authored-by: Piotr Jurewicz <pjurewicz93@gmail.com>

--- examples/functional/lib/project_management/issue.rb
+++ examples/functional/lib/project_management/issue.rb
@@ -2,51 +2,53 @@ module ProjectManagement
   module Issue
     InvalidTransition = Class.new(StandardError)
 
-    def self.open(state)
-      if state.status
-        InvalidTransition.new
-      else
-        IssueOpened.new(data: { issue_id: state.id })
+    class << self
+      def open(state)
+        if state.status
+          InvalidTransition.new
+        else
+          IssueOpened.new(data: { issue_id: state.id })
+        end
       end
-    end
 
-    def self.resolve(state)
-      if %i[open in_progress reopened].include? state.status
-        IssueResolved.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
+      def resolve(state)
+        if %i[open in_progress reopened].include? state.status
+          IssueResolved.new(data: { issue_id: state.id })
+        else
+          InvalidTransition.new
+        end
       end
-    end
 
-    def self.close(state)
-      if %i[open in_progress resolved reopened].include? state.status
-        IssueClosed.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
+      def close(state)
+        if %i[open in_progress resolved reopened].include? state.status
+          IssueClosed.new(data: { issue_id: state.id })
+        else
+          InvalidTransition.new
+        end
       end
-    end
 
-    def self.reopen(state)
-      if %i[resolved closed].include? state.status
-        IssueReopened.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
+      def reopen(state)
+        if %i[resolved closed].include? state.status
+          IssueReopened.new(data: { issue_id: state.id })
+        else
+          InvalidTransition.new
+        end
       end
-    end
 
-    def self.stop(state)
-      if %i[in_progress].include? state.status
-        IssueProgressStopped.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
+      def stop(state)
+        if %i[in_progress].include? state.status
+          IssueProgressStopped.new(data: { issue_id: state.id })
+        else
+          InvalidTransition.new
+        end
       end
-    end
 
-    def self.start(state)
-      if %i[open reopened].include? state.status
-        IssueProgressStarted.new(data: { issue_id: state.id })
-      else
-        InvalidTransition.new
+      def start(state)
+        if %i[open reopened].include? state.status
+          IssueProgressStarted.new(data: { issue_id: state.id })
+        else
+          InvalidTransition.new
+        end
       end
     end
   end
