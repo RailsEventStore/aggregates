2f06875e6335f99ef5c7b768b81040f838fd5a49
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-26 12:11:18 +0100

    Refactor

--- examples/repository/lib/project_management/repository.rb
+++ examples/repository/lib/project_management/repository.rb
@@ -1,23 +1,19 @@
 module ProjectManagement
   class Repository
     def initialize(event_store)
-      @store = event_store
+      @event_store = event_store
     end
 
     def load(id)
-      store.read.stream(stream_name(id)).to_a
+      @event_store.read.stream(stream_name(id))
     end
 
-    def save(id, changes)
-      store.publish(changes, stream_name: stream_name(id))
+    def save(id, events)
+      @event_store.append(events, stream_name: stream_name(id))
     end
 
     private
 
-    attr_reader :store
-
-    def stream_name(id)
-      "Issue$#{id}"
-    end
+    def stream_name(id) = "Issue$#{id}"
   end
 end
