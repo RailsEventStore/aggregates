108b6f2b70bc9bcc2e14b5f35771703bc529f331
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-26 12:18:49 +0100

    Dead code

--- examples/yield_based/lib/project_management.rb
+++ examples/yield_based/lib/project_management.rb
@@ -3,17 +3,3 @@ require_relative "aggregate_root"
 require_relative "aggregate_repository"
 require_relative "project_management/handler"
 require_relative "project_management/issue"
-
-module ProjectManagement
-  class Configuration
-    def call(event_store, command_bus)
-      handler = Handler.new(event_store)
-      command_bus.register(CreateIssue, handler.public_method(:create))
-      command_bus.register(ReopenIssue, handler.public_method(:reopen))
-      command_bus.register(ResolveIssue, handler.public_method(:resolve))
-      command_bus.register(CloseIssue, handler.public_method(:close))
-      command_bus.register(StartIssueProgress, handler.public_method(:start))
-      command_bus.register(StopIssueProgress, handler.public_method(:stop))
-    end
-  end
-end
