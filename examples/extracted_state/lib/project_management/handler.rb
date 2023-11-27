896411b94b7dfa13239628f05d4bb18dca79680e
Paweł Pacana <pawel.pacana@gmail.com>
2023-11-28 00:11:00 +0100

    Extract repository

--- examples/extracted_state/lib/project_management/handler.rb
+++ examples/extracted_state/lib/project_management/handler.rb
@@ -1,6 +1,8 @@
 module ProjectManagement
   class Handler
-    def initialize(event_store) = @event_store = event_store
+    def initialize(event_store)
+      @repository = Repository.new(event_store)
+    end
 
     def call(cmd)
       case cmd
@@ -30,18 +32,12 @@ module ProjectManagement
 
     private
 
-    def stream_name(id) = "Issue$#{id}"
-
     def with_aggregate(id)
-      state =
-        @event_store
-          .read
-          .stream(stream_name(id))
-          .reduce(IssueState.initial(id)) { |state, event| state.apply(event) }
+      state = @repository.load(id, IssueState.initial(id))
+      issue = Issue.new(state)
+      yield issue
 
-      yield issue = Issue.new(state)
-
-      @event_store.append(issue.changes, stream_name: stream_name(id))
+      @repository.store(id, issue.changes)
     end
   end
 end
